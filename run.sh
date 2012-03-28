99cf2c7de5b83bf313a81dfc617636deaa6ef06e
Serafin Sedano <serafin.sedano@abiquo.com>
2012-03-28 21:02:37 +0200

    Added the location of the mplayer bin

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-screen -L -d -m -S elfari-webservice /Users/admin/.rvm/rubies/ruby-1.9.3-p125/bin/ruby elfari-webservice.rb
-screen -L -d -m -S elfari /Users/admin/.rvm/rubies/ruby-1.9.3-p125/bin/ruby elfari.rb
+screen -L -d -m -S elfari-webservice ruby elfari-webservice.rb
+screen -L -d -m -S elfari ruby elfari.rb
+
 

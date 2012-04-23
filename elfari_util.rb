dd174bff2bafd23042c1fa150da72d2fc9fb6eab
Serafin Sedano <serafin.sedano@abiquo.com>
2012-04-23 17:21:53 +0200

    The class name is now the proper one. The regular expression nows admits the format XXh:YYs:WWm

--- elfari_util.rb
+++ elfari_util.rb
@@ -1,14 +1,13 @@
-Class ElfariUtil
+class ElFariUtil
     def self.extract_seek_time(expr = "")
         seconds = 0
-        hours = /(\+d)h/.match(expr)
-        minutes = /(\+d)m/.match(expr)
-        seconds_expr = /(\d+)s/.match(expr)
-        seconds += hours * 3600 unless hours.nil?
-        seconds += minutes * 60 unless minutes.nil?
-        seconds += seconds_expr unless seconds_expr.nil?
-        
+        matcher = /(\s*(\d+)\s?[h])?[:]?(\s*(\d+)\s?[m])?[:]?(\s*(\d+)\s?[s])?/.match(expr)
+        hours = matcher.values_at(2)
+        minutes = matcher.values_at(4)
+        seconds_expr = matcher.values_at(6)
+        seconds += hours.first.to_i * 3600 unless hours.nil?
+        seconds += minutes.first.to_i * 60 unless minutes.nil?
+        seconds += seconds_expr.first.to_i unless seconds_expr.nil?
         return seconds
     end
-    
-end
+end 
